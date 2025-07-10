caa5179c10e297e1e37fe3323fb1c552d63c2900
NiceAdam25 <88014591+AdamHarish25@users.noreply.github.com>
2025-07-10 13:45:52 +0700

    Delete backend directory

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-import app from "./app.js";
-
-dotenv.config();
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server listening on http://localhost:${process.env.PORT}`);
-});
--- backend/utils/token/getToken.js
+++
@@ -1,9 +0,0 @@
-import getAccessToken from "./getAccessToken.js";
-import getRefreshToken from "./getRefreshToken.js";
-
-export default function getToken(payload) {
-    return {
-        accessToken: getAccessToken(payload),
-        refreshToken: getRefreshToken(payload),
-    };
-}
