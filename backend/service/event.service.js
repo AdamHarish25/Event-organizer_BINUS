fa600a9ba318922daee6e0ef383f46a407aaa0e2
Rizal Ramadhan <rizalramadhan2214@gmail.com>
2025-07-29 12:20:21 +0700

    menghapus logika pagination dan menggantinya dengan categorization

--- backend/service/event.service.js
+++ backend/service/event.service.js
@@ -56,6 +56,7 @@ export const getCategorizedEventsService = async (EventModel) => {
         return finalResult;
     } catch (error) {
         console.error("Gagal mengambil data event terkategori:", error);
+        throw error;
     }
 };
 
