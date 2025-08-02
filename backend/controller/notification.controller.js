16e1442306a8106eab435c471e0d94846002d27a
Rizal Ramadhan <rizalramadhan2214@gmail.com>
2025-08-02 07:30:14 +0700

    menambahkan API route dan controller untuk mengambil data notifikasi event

---
+++ backend/controller/notification.controller.js
@@ -0,0 +1,8 @@
+import { getNotificationService } from "../service/notification.service.js";
+
+export const getNotification = async (req, res, next) => {
+    try {
+    } catch (error) {
+        next(error);
+    }
+};
---
+++ backend/routes/notification.routes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import { accessTokenValidator } from "../middleware/tokenValidator.middleware.js";
+import { getNotification } from "../controller/notification.controller.js";
+
+dotenv.config({ path: "../.env" });
+const { ACCESS_JWT_SECRET } = process.env;
+const router = express.Router();
+
+router.get(
+    "/get-notification",
+    accessTokenValidator(ACCESS_JWT_SECRET),
+    getNotification
+);
